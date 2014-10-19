3746e221d9f767a8149a57a0d97ffdba8712f14f
Peter Tribble <peter.tribble@gmail.com>
2014-10-19 15:30:58 +0100

    Rev to milestone 12

--- repo2svr4.sh
+++ repo2svr4.sh
@@ -3,7 +3,7 @@
 # convert an ips package to svr4, from an on-disk repo
 #
 
-PKG_VERSION="0.11.0"
+PKG_VERSION="0.12.0"
 THOME=/packages/localsrc/Tribblix
 GATEDIR=/export/home/ptribble/Illumos/illumos-gate
 
