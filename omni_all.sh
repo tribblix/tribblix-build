e8be67ce18e4fd293ea4a7a8381fa342bdf8ef43
Peter Tribble <peter.tribble@gmail.com>
2018-02-18 15:27:49 +0000

    Bump LX version to 20.3

--- omni_all.sh
+++ omni_all.sh
@@ -5,7 +5,7 @@
 #
 # these ought to be args
 #
-PKG_VERSION="0.20lx.2"
+PKG_VERSION="0.20lx.3"
 THOME=/packages/localsrc/Tribblix
 GATEDIR=/export/home/ptribble/Illumos/omnitribblix
 DSTDIR=/var/tmp/omni-pkgs
