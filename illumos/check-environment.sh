a3c02dae10453f071ab64a309ce9524e8aad4cef
Peter Tribble <peter.tribble@gmail.com>
2019-10-06 16:08:57 +0100

    Check for a sane environment when building the gate

---
+++ illumos/check-environment.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+#
+# checks that the environment is suitable for building the gate
+#
+
+#
+# hold the status here
+# we run all checks, so as to report all errors
+#
+STATUS=0
+
+#
+# basic sanity, this will only work on Tribblix
+#
+if [ ! -d /usr/lib/zap ]; then
+    echo "ERROR: is this really Tribblix?"
+    STATUS=1
+fi
+
+#
+# check for installed overlays
+# we need develop and illumos-build
+# we need java or java8
+#
+ODIR=/var/sadm/overlays/installed/
+if [ ! -f ${ODIR}/develop ]; then
+    echo "ERROR: the develop overlay must be installed"
+    echo "As root, run the command"
+    echo "zap install-overlay develop"
+    STATUS=1
+fi
+if [ ! -f ${ODIR}/illumos-build ]; then
+    echo "ERROR: the illumos-build overlay must be installed"
+    echo "As root, run the command"
+    echo "zap install-overlay illumos-build"
+    STATUS=1
+fi
+if [ ! -f ${ODIR}/java ]; then
+    if [ ! -f ${ODIR}/java8 ]; then
+	echo "ERROR: the java or java8 overlay must be installed"
+	echo "As root, run the command"
+	echo "zap install-overlay java8"
+	STATUS=1
+    fi
+fi
+
+#
+# /usr/bin/cpp must be deleted
+#
+if [ -f /usr/bin/cpp ]; then
+    echo "ERROR: /usr/bin/cpp should not be present"
+    echo "As root, run the command"
+    echo "rm -f /usr/bin/cpp"
+    STATUS=1
+fi
+
+#
+# /usr/bin/gxgettext must exist
+#
+if [ ! -f /usr/bin/gxgettext ]; then
+    echo "ERROR: /usr/bin/gxgettext should be present"
+    echo "As root, run the command"
+    echo "ln -s ../gnu/bin/xgettext /usr/bin/gxgettext"
+    STATUS=1
+fi
+
+#
+# if tests fail, we exit with an error to alert our caller
+#
+
+exit $STATUS
