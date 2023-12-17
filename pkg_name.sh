3350ced25133643e10fdebbb5199b0ac6cef6f09
Peter Tribble <peter.tribble@gmail.com>
2023-12-17 16:40:16 +0000

    Add the CDDL header

--- pkg_name.sh
+++ pkg_name.sh
@@ -1,5 +1,22 @@
 #!/bin/ksh
 #
+# {{{ CDDL HEADER
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source. A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+#
+# }}}
+#
+# Copyright 2023 Peter Tribble
+#
+
+#
 # convert an IPS package name to an SVR4 name
 #
 # SUNW old-fashioned names don't get converted, this should only be
